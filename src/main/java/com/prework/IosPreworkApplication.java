d5d741f00db245b7000729cfe8fccf793963bede
usingh <uttam_singh@intuit.com>
2017-07-09 12:39:51 +0530

    Commit project

---
+++ src/main/java/com/prework/IosPreworkApplication.java
@@ -0,0 +1,12 @@
+package com.prework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosPreworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IosPreworkApplication.class, args);
+	}
+}
